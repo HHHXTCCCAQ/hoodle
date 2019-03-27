6b6fb6d5333aa12b223324a6ac9979c6e5388de1
HHHXTCCCAQ <2507200459@qq.com>
2019-03-27 19:01:16 +0800

    Update _2019-3-27

---
+++ Pinball Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Wait for me, I don't want to let you down
+//love you into disease, but no medicine can.
+//Created By HeXiaoTao
+public class Ball : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Pinball Game/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Wait for me, I don't want to let you down
+//love you into disease, but no medicine can.
+//Created By HeXiaoTao
+public class PlayAnimation : MonoBehaviour
+{
+
+    // Use this for initialization
+    private Animation animation;
+    public ParticleSystem particleSystemprefab;
+    private ParticleSystem particleSystem;
+    void Start()
+    {
+        animation = this.GetComponent<Animation>();
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag.Equals("Ball"))
+        {
+            if (particleSystemprefab != null)
+            {
+                particleSystem = Instantiate(particleSystemprefab, collision.transform.position, Quaternion.Euler(new Vector3(90,0,0)));
+                particleSystem.Play();
+            }
+            if (animation != null)
+                animation.Play();
+        }
+        Destroy(particleSystem, 0.5f);
+    }
+
+
+}
