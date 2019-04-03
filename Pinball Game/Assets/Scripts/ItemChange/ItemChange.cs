720c480f66222659875d4515c4ed3e414c39b272
HHHXTCCCAQ <2507200459@qq.com>
2019-04-03 19:30:44 +0800

    2019-4-3_update

---
+++ Pinball Game/Assets/Scripts/ItemChange/ItemChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Wait for me, I don't want to let you down
+//love you into disease, but no medicine can.
+//Created By HeXiaoTao
+public class ItemChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+    float angle = 0;
+    // Update is called once per frame
+    void Update () {
+        if (!DelayStartGame._instance.startGame)
+            return;
+        angle -= Time.deltaTime * 20;
+        transform.rotation = Quaternion.Euler(new Vector3(0,0, angle));
+	}
+}
